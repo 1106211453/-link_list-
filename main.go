7b8ce8ebd4e1ae908a4f9e4dc8a1833200045426
1106211453 <1106211453@qq.com>
2023-09-24 09:47:10 +0000

    sfsd

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("djdjdjd")
+	// %d 表示整型数字，%s 表示字符串
+	var stockcode = 123
+	var enddate = "2020-12-31"
+	var url = "Code= %d &endDate= %s "
+	var target_url = fmt.Sprintf(url, stockcode, enddate)
+	fmt.Println(target_url)
 }

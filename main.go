32746a591e941557883e7d4ae25e2c50eb64b4cc
1106211453 <1106211453@qq.com>
2023-09-24 09:50:31 +0000

    sada

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	// %d 表示整型数字，%s 表示字符串
 	var stockcode = 123
-	var enddate = "2020-12-31"
+	var enddate = "2020-12-31 %d"
 	var url = "Code= %d &endDate= %s "
 	var target_url = fmt.Sprintf(url, stockcode, enddate)
 	fmt.Println(target_url)
